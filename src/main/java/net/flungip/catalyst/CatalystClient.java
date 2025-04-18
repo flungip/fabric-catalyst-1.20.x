7aa60f268819645ca1461f8087579a9f1463840b
flungip <132852748+flungip@users.noreply.github.com>
2025-04-18 13:46:44 -0400

    Initial commit

---
+++ src/main/java/net/flungip/catalyst/CatalystClient.java
@@ -0,0 +1,10 @@
+package net.flungip.catalyst;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CatalystClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/flungip/catalyst/CatalystDataGenerator.java
@@ -0,0 +1,11 @@
+package net.flungip.catalyst;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CatalystDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
